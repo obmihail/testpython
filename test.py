19b3ad1cf1fdbf24c5955606c5f5bdca85bd8638
mihailob <mihailob@localhost.localdomain>
2018-08-30 12:47:07 +0300

    <img src=x onerror=alert()>

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#test <img src=x onerror=alert()>
+#<![CDATA[<img src=x onerror=alert()>]]>
+#<![CDATA[<a href="javascript:alert()" src=x onerror=alert()>]]>LINK</a>
 import requests
 r = requests.get('https://yaqs.googleplex.com/eng')
 print r
